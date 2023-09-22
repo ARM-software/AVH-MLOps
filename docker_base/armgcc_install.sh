8e1c6daefe1685bb41755540e3fc20e746b7dcc8
Matthias Hertel <matthias.hertel@arm.com>
2023-09-22 17:55:06 +0200

    Update armgcc_install.sh

--- docker_base/armgcc_install.sh
+++ docker_base/armgcc_install.sh
@@ -1,4 +1,4 @@
 wget https://artifacts.keil.arm.com/arm-none-eabi-gcc/12.3.1/arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -nv
-tar -xf arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -C /home/arm_mlops_docker
+tar -xjf arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -C /home/arm_mlops_docker
 echo 'export PATH="$PATH:/home/arm_mlops_docker/arm-gnu-toolchain-x86_64-arm-none-eabi/bin"' >> /home/arm_mlops_docker/.bashrc
 chown -R arm_mlops_docker /home/arm_mlops_docker/arm-gnu-toolchain-x86_64-arm-none-eabi
