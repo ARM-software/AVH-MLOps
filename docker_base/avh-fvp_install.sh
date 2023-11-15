7a457077beef52aca2f5df4f936dd1f1eaccf9c1
Matthias Hertel <matthias.hertel@arm.com>
2023-11-15 09:33:49 +0100

    Fix libpython incompatiblities

--- docker_base/avh-fvp_install.sh
+++ docker_base/avh-fvp_install.sh
@@ -1,4 +1,5 @@
 wget https://artifacts.keil.arm.com/avh/11.22.39/avh-fvp-linux-amd64.tar.gz -nv
 mkdir /home/arm_mlops_docker/avh-fvp-11.22.39
 tar -xvf avh-fvp-linux-amd64.tar.gz -C /home/arm_mlops_docker/avh-fvp-11.22.39
+rm /home/arm_mlops_docker/avh-fvp-11.22.39/avh-linux-x86/bin/models/libpython3.9.so.1.0
 chmod 0755 /home/arm_mlops_docker/avh-fvp-11.22.39 -R
