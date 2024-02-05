31aa0abd68de95ab200eaa348d03f47f74dbcfb6
Matthias Hertel <matthias.hertel@arm.com>
2024-02-05 14:18:25 +0100

    New artifactory links (#11)
    * Update armclang_install.sh
    
    * Update armgcc_install.sh
    
    * Update avh-fvp_install.sh
    
    * Update cmsistoolbox_install.sh
    
    * Update README.md

--- docker_base/armclang_install.sh
+++ docker_base/armclang_install.sh
@@ -1,4 +1,4 @@
-wget https://artifacts.keil.arm.com/arm-compiler/6.20/21/standalone-linux-x86_64-rel.tgz -nv
+wget https://artifacts.tools.arm.com/arm-compiler/6.20/21/standalone-linux-x86_64-rel.tgz -nv
 mkdir ./ArmCompilerforEmbedded6.20
 tar xvfz ./standalone-linux-x86_64-rel.tgz -C ./ArmCompilerforEmbedded6.20
 chown -R arm_mlops_docker ./ArmCompilerforEmbedded6.20
