af5f6cf7c44356d67ab1935df14a7e5c6914fff1
Matthias Hertel <matthias.hertel@arm.com>
2025-01-15 09:51:55 +0100

    Update armgcc_install.sh (#13)

--- docker_base/armgcc_install.sh
+++ docker_base/armgcc_install.sh
@@ -1,3 +1,4 @@
 wget https://artifacts.tools.arm.com/arm-none-eabi-gcc/12.3.1/arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -nv
+apt-get install -y --no-install-recommends bzip2
 tar -xf arm-gnu-toolchain-x86_64-arm-none-eabi.tar.bz2 -C /home/arm_mlops_docker
 chown -R arm_mlops_docker /home/arm_mlops_docker/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/
